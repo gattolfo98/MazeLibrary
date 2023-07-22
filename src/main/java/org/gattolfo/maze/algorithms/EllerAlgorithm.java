9d89b2c861f6949df2fceccb27a899d2bf497d2e
gattolfo <gattolfo2.0@gmail.com>
2023-07-22 13:38:40 +0200

    Working on EllerAlgorithm.java

--- src/main/java/org/gattolfo/maze/algorithms/EllerAlgorithm.java
+++ src/main/java/org/gattolfo/maze/algorithms/EllerAlgorithm.java
@@ -7,15 +7,28 @@ public class EllerAlgorithm extends MazeGenerator{
 
     public EllerAlgorithm(int height, int width) {
         super();
-        throw new UnsupportedOperationException("Not supported yet.");
+        map = new Cell[height][width];
+        for(int i=0;i<height;i++){
+            for(int z=0;z<width;z++){
+                map[i][z] = new Cell(i,z,false);
+                if(i==0)
+                    map[i][z].setWallUp(true);
+                if(i==height-1)
+                    map[i][z].setWallDown(true);
+                if(z==0)
+                    map[i][z].setWallLeft(true);
+                if(z==width-1)
+                    map[i][z].setWallRight(true);
+            }
+        }
 
     }
 
     @Override
     public Cell[][] generateMaze() {
 
-        throw new UnsupportedOperationException("Not supported yet.");
 
+        return map;
     }
 
     private void generateRow(int rowHeight){
